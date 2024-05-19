acef11596efa1ca3360b023a44b4de3c6266618f
pklemy <l0013.k0013@gmail.com>
2024-05-19 13:26:38 +0000

    [fix] letter_opener default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,9 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost", port: "3000" }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
